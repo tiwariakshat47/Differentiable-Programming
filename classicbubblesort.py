9de942a521207173e6d40a3088c4d19a62e84e65
deckerkrogh <deckerbjorn@gmail.com>
2025-02-05 17:31:07 -0800

    small changes

--- classicbubblesort.py
+++ classicbubblesort.py
@@ -16,6 +16,16 @@ def bubble_sort(arr):
     return arr
 
 
+# def bubble_sort(arr):
+#     for i in range(len(arr)):
+#         for j in range(len(arr)-i-1):
+#             tmp = a[j]
+
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#     return arr
+
+
 def apply_gaussian_to_array(array, sigma=1.0):
     size = len(array)
     distributions = []
@@ -30,7 +40,7 @@ def apply_gaussian_to_array(array, sigma=1.0):
 #Creating 4 functions razvan suggested: softindex (done above), softget, softswap, softset
 
 def softget(arr, index_distribution):
-    return torch.dot(index_distribution.clone().detach().requires_grad_(True), arr)
+    return torch.dot(index_distribution, arr)
 
 
 
@@ -41,7 +51,7 @@ def softset(arr, index_distribution, value):
     # then do --> i[0] --> new_arr[0]=(1−0.2)⋅1+0.2⋅10=0.8⋅1+2.0= 2.8
     # i[1] --> new_arr[1]=(1−0.5)⋅2+0.5⋅10=0.5⋅2+5.0= 6.0
     # i[2] --> new_arr[2]=(1−0.3)⋅3+0.3⋅10=0.7⋅3+3.0= 5.1
-    index_distribution = index_distribution.clone().detach().requires_grad_(True)
+    # index_distribution = index_distribution.clone().detach()
     return arr * (1 - index_distribution) + index_distribution * value
 
 
@@ -77,13 +87,17 @@ def soft_bubble_sort(arr, distributions, y, iterations=1):
         i_range = round(torch.sub(torch.Tensor([size]), y).item())
         for i in range(i_range):
             dist1 = distributions[i]
-            dist2 = distributions[round(torch.add(torch.Tensor([i]), y).item())]
-
+            dist2_i = torch.add(torch.Tensor([i]), y)
+            # print(y.grad)
+            dist2 = distributions[round(dist2_i.item())]
             val1 = softget(arr, dist1)
             val2 = softget(arr, dist2)
+            # print(arr.grad)
 
             if val1 > val2:
                 arr = softswap(arr, dist1, dist2)
+    
+    # print(arr.grad_fn)
 
     return arr
 
@@ -91,26 +105,37 @@ def soft_bubble_sort(arr, distributions, y, iterations=1):
 
 
 array = torch.tensor([30.0, 1.0, 100.0, 5.0, 2.0, 75.0, 50.0, 10.0, 40.0, 60.0], requires_grad=False)  # keep this as false so we don't modify actual array
+# [1, 2, 5, 10, 30, 40, 50, 60, 75, 100]
+# [4, 0, 9, ]
+
+# [0, 0, 0, 0, 1, 0, 0, 0, 0]
+# [1, 0, 0, 0, 0, 0, 0, 0, 0]
 
 sigma = 0.3
 distributions = apply_gaussian_to_array(array, sigma)
 
 classical_sorted_array = torch.tensor(bubble_sort(array.clone().tolist()), dtype=torch.float32)
 
-dist_param = torch.tensor(1.0, requires_grad=True)
+dist_param = torch.tensor(2.0, requires_grad=True)
 optimizer = torch.optim.SGD([dist_param], lr=0.3)
 
 losses = []
 soft_sorted_arrays = []
 
-num_epochs = 5
+num_epochs = 4
 
 for epoch in range(num_epochs):
     optimizer.zero_grad()
 
     soft_sorted_array = soft_bubble_sort(array, distributions, y=dist_param, iterations=5)
     loss = compute_loss(soft_sorted_array, classical_sorted_array)
+    # print(loss)
+    loss.register_hook(lambda grad: print(f'loss grad: {grad}'))
     loss.backward()
+    print(f'soft_sorted_array: {soft_sorted_array.grad}')
+    print(f'soft_sorted_array grad_fn: {soft_sorted_array.grad_fn}')
+    # print(f'loss: {loss}')
+    # print(f'loss grad: {loss.grad}')
     optimizer.step()
     # print(f'dist_param: {dist_param}')
     # print(f'dist_param grad: {dist_param.grad}')
